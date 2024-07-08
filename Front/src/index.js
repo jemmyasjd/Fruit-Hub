00ce371a5d25ea792f19c6e2372f13f44eafaf50
jemmyasjd <jemmy33.jd@gmail.com>
2024-07-08 23:17:07 +0530

    some changes in index

--- Front/src/index.js
+++ Front/src/index.js
@@ -2,20 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ClerkProvider } from '@clerk/clerk-react'
+import { ClerkProvider } from '@clerk/clerk-react';
 
+const PUBLISHABLE_KEY = process.env.REACT_APP_PUBLISH_KEY;
 
-const PUBLISHABLE_KEY = process.env.REACT_APP_PUBLISH_KEY
-console.log(PUBLISHABLE_KEY)
-
-// if (!PUBLISHABLE_KEY) {
-//   throw new Error("Missing Publishable Key")
-// }
+if (!PUBLISHABLE_KEY) {
+  console.error("Missing Publishable Key");
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ClerkProvider publishableKey='pk_test_d2hvbGUtbW9uc3Rlci04NS5jbGVyay5hY2NvdW50cy5kZXYk'>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY || 'pk_test_d2hvbGUtbW9uc3Rlci04NS5jbGVyay5hY2NvdW50cy5kZXYk'}>
       <App />
     </ClerkProvider>
   </React.StrictMode>
