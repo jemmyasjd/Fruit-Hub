ae889ef7711d016ef6e2f68f996da16369e527f9
jemmyasjd <jemmy33.jd@gmail.com>
2024-07-08 23:21:58 +0530

    removed the key

--- Front/src/index.js
+++ Front/src/index.js
@@ -4,16 +4,11 @@ import './index.css';
 import App from './App';
 import { ClerkProvider } from '@clerk/clerk-react';
 
-const PUBLISHABLE_KEY = process.env.REACT_APP_PUBLISH_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  console.error("Missing Publishable Key");
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY || 'pk_test_d2hvbGUtbW9uc3Rlci04NS5jbGVyay5hY2NvdW50cy5kZXYk'}>
+    <ClerkProvider publishableKey={'pk_test_d2hvbGUtbW9uc3Rlci04NS5jbGVyay5hY2NvdW50cy5kZXYk'}>
       <App />
     </ClerkProvider>
   </React.StrictMode>
