32643cfed83e02ca4e81724d646062beae8cc542
jemmyasjd <jemmy33.jd@gmail.com>
2024-07-08 22:20:40 +0530

    first commit

---
+++ Backend/controllers/Payment.js
@@ -0,0 +1,43 @@
+const stripe = require('stripe')('sk_test_51O2atUSH0CmyMvGVzrTne3sfW5v575zZ6b0T5WqtLy80S1SjgHQqIK6pb1mORSDp1qdK5GQVKSQ7YBuJa67LR8OM00gO26Pkyx');
+
+
+async function Payment(req,res){
+    try {
+        const { totalAmount, email } = req.body;
+    
+        if (!totalAmount) {
+          return res.status(400).json({ error: 'Total amount is required' });
+        }
+    
+        const product = await stripe.products.create({
+          name: "Total Amount",
+          images: ["https://cdn-icons-png.freepik.com/512/3/3729.png"]
+        });
+    
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: totalAmount * 100, // Convert to cents/paise
+          currency: 'inr',
+        });
+    
+        const session = await stripe.checkout.sessions.create({
+          line_items: [
+            {
+              price: price.id,
+              quantity: 1,
+            }
+          ],
+          mode: 'payment',
+          success_url: 'http://localhost:3000',
+          cancel_url: 'http://localhost:5000/cancel',
+          customer_email: email,
+        });
+    
+        res.status(200).json({ url: session.url });
+      } catch (error) {
+        console.error('Error creating payment session:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+}
+
+module.exports= {Payment};
---
+++ Backend/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const {connectDB} = require('./connection.js');
+const cors = require('cors');
+require('dotenv').config();
+const PayemntRouter = require('./routes/payment');
+const ProductRouter = require('./routes/product');
+const OderRouter = require('./routes/order');
+
+
+const app = express();
+
+connectDB('mongodb+srv://jemmy33jd:Mongodb.33@cluster0.1auwi0e.mongodb.net/fruits_db');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.use('/payment', PayemntRouter);
+app.use('/product', ProductRouter);
+app.use('/order', OderRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});  
+
+app.post('/test', (req, res) => {
+    console.log(req.body);
+    const userId = req.auth.userId;
+    console.log('User ID:', userId);
+    res.json({url: 'http://localhost:3000/success'});
+});
+
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
+});
