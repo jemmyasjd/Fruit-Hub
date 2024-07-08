24940a6890383dd4bc9c91db61cb5c530ad65e3b
jemmyasjd <jemmy33.jd@gmail.com>
2024-07-08 23:30:04 +0530

    some changes

--- Front/src/index.js
+++ Front/src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ClerkProvider } from '@clerk/clerk-react';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <ClerkProvider publishableKey={'pk_test_d2hvbGUtbW9uc3Rlci04NS5jbGVyay5hY2NvdW50cy5kZXYk'}> */}
       <App />
-    {/* </ClerkProvider> */}
   </React.StrictMode>
 );
